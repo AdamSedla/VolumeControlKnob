b80e57ab7b0578af0c24aa46eb352a65c7d92ec3
Adam Sedláček <sedlaad1@fit.cvut.cz>
2024-04-26 00:23:54 +0200

    tE: adds file header

--- testSoftware/fullTest/fullTest.ino
+++ testSoftware/fullTest/fullTest.ino
@@ -7,3 +7,28 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_GFX.h>
+#include <Arduino_ST7789_Fast.h>
+
+//SD_reader
+#define SD_CS 4
+//display
+#define TFT_DC 7
+#define TFT_RST 8
+#define SCR_WD 240
+#define SCR_HT 240
+//buttons
+#define SYNC 0
+#define SETT 0
+#define APP_CHOOSE 0
+//rotary
+#define ROT_CLK 0
+#define ROT_DT 0
+#define ROT_SW 0
+//linear
+#define LINEAR A0
+
+Arduino_ST7789 lcd = Arduino_ST7789(TFT_DC, TFT_RST);
+const int chipSelect = SD_CS;
