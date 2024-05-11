abfd01ad818bc2db31d08b291d97f64868687d05
Adam Sedláček <sedlaad1@fit.cvut.cz>
2024-05-11 19:17:45 +0200

    adds header

--- software/main/main.ino
+++ software/main/main.ino
@@ -1,4 +1,34 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_GFX.h>
+#include <Arduino_ST7789_Fast.h>
 #include "discord.h"
+#include "joystick.h"
+#include "spotify.h"
+
+//SD_reader
+#define SD_CS 4
+//display
+#define TFT_DC 7
+#define TFT_RST 8
+#define SCR_WD 240
+#define SCR_HT 240
+//buttons
+#define SYNC 5
+#define SETT 3
+#define APP_CHOOSE 2
+//rotary
+#define ROT_CLK 6
+#define ROT_DT 9
+#define ROT_SW 10
+//linear
+#define LINEAR A0
+
+Arduino_ST7789 lcd = Arduino_ST7789(TFT_DC, TFT_RST);
+const int chipSelect = SD_CS;
+
+Sd2Card card;
+
 void drawDiscord() {
   lcd.fillScreen(BLACK);
   size_t imagePos = 0;
