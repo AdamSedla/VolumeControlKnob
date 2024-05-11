cc937ad2d2da1c2631e86daff91770784e578b39
Adam Sedláček <sedlaad1@fit.cvut.cz>
2024-05-11 19:16:18 +0200

    adds function for joystick icon

--- software/main/main.ino
+++ software/main/main.ino
@@ -24,3 +24,29 @@ void drawDiscord() {
     }
   }  
 }
+void drawJoystick() {
+  lcd.fillScreen(BLACK);
+  size_t imagePos = 0;
+
+      /*
+        1 - imagePos + 2*(imagePos-1)
+        2 - imagePos + imagePos*2-1 
+        3 - 3*imagePos
+        */    
+
+     for(size_t i = 0; i<80;i++){
+    for(size_t j = 0;j<80;j++){
+      lcd.drawPixel(j + 2*(j-1) , i + i*2-1 , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j + 2*(j-1) , i + 2 * (i-1) , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j + 2*(j-1) , i*3 , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j + j*2-1   , i + i*2-1 , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j + j*2-1   , i + 2 * (i-1) , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j + j*2-1   , i*3 , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j * 3       , i + i*2-1 , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j * 3       , i + 2 * (i-1) , pgm_read_word(&joystick[imagePos]));
+      lcd.drawPixel(j * 3       , i*3 , pgm_read_word(&joystick[imagePos]));    
+    
+      imagePos++;
+    }
+  } 
+}
